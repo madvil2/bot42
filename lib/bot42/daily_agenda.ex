d842fcf15c1829cc8a2c7a9e0340253700f4f80e
KONSTANTIN KAIMOV <ikaimov98@gmail.com>
2023-12-10 23:51:57 +0100

    fix

--- lib/bot42/daily_agenda.ex
+++ lib/bot42/daily_agenda.ex
@@ -8,88 +8,56 @@ defmodule Bot42.DailyAgenda do
 
   @spec formated_today_events :: {:ok, [map()] | []} | {:error, term()}
   def formated_today_events do
-    case today_events_from_calendar() do
+    case events_from_calendar() do
       {:ok, events} ->
-        IO.inspect(events, label: "События перед форматированием")
-        {:ok, format_events(events)}
+        formated_today_events =
+          events
+          |> filter_today_events()
+          |> format_events()
+
+        {:ok, formated_today_events}
 
       {:error, _} = error ->
         error
     end
   end
 
-
-  @spec today_events_from_calendar :: {:ok, [map()] | []} | {:error, :external_api_error | term()}
-  defp today_events_from_calendar do
+  @spec events_from_calendar :: {:ok, [map()] | []} | {:error, :external_api_error | term()}
+  defp events_from_calendar do
     case HTTPoison.get(daily_agenda_url()) do
-      {:ok, %{status_code: 200, body: body}} ->
-        parse_ical_data(body)
-
-      response ->
-        IO.inspect(response, label: "Неудачный ответ от календаря")
-        {:error, :external_api_error}
+      {:ok, %{status_code: 200, body: body}} -> parse_ical_data(body)
+      _ -> {:error, :external_api_error}
     end
   end
 
+  @spec parse_ical_data(String.t()) :: {:ok, [map()] | []} | {:error, :invalid_data}
   defp parse_ical_data(ical_data) do
     case ICalendar.from_ics(ical_data) do
-      {:ok, calendars} ->
-        filter_and_process_events(calendars)
-
-      events when is_list(events) ->
-        if Enum.all?(events, &is_struct(&1, ICalendar.Event)) do
-          filter_and_process_events_direct(events)
-        else
-          {:error, :invalid_data}
-        end
-
-      _ ->
-        {:error, :invalid_data}
+      events when is_list(events) -> {:ok, events}
+      _ -> {:error, :invalid_data}
     end
   end
 
-  defp filter_and_process_events(calendars) do
+  @spec filter_today_events([map()] | []) :: [map()] | []
+  defp filter_today_events(events) do
     today = Date.utc_today()
-    current_time = DateTime.utc_now()
 
-    events =
-      Enum.flat_map(calendars, fn calendar ->
-        Enum.filter(calendar.events, fn event ->
-          date_comparison = Date.compare(event.dtstart, today)
-          (date_comparison in [:eq, :lt]) and event.dtend > current_time
-        end)
-      end)
-
-    case events do
-      [] -> {:ok, "No events"}
-      _ -> {:ok, events}
-    end
-  end
-
-  defp filter_and_process_events_direct(events) do
-    today = Date.utc_today()
-    current_time = DateTime.utc_now()
-
-    filtered_events =
-      Enum.filter(events, fn event ->
-        date_comparison = Date.compare(event.dtstart, today)
-        (date_comparison in [:eq, :lt]) and event.dtend > current_time
-      end)
-
-    case filtered_events do
-      [] -> {:ok, "No events"}
-      _ -> {:ok, filtered_events}
-    end
+    Enum.filter(events, fn event -> DateTime.to_date(event.dtstart)== today end)
   end
 
   @spec format_events([map()] | []) :: String.t()
   defp format_events(events) do
-    "Today's Agenda:\n" <>
-      Enum.map_join(events, "\n", fn event ->
-        start_time = Calendar.strftime(event.dtstart, "%H:%M")
-        end_time = Calendar.strftime(event.dtend, "%H:%M")
+    case events do
+      [] -> "Today's Agenda:\nNo events for today"
 
-        "#{event.summary}: from #{start_time} to #{end_time}"
-      end)
+      events ->
+        "Today's Agenda:\n" <>
+          Enum.map_join(events, "\n", fn event ->
+            start_time = Calendar.strftime(event.dtstart, "%H:%M")
+            end_time = Calendar.strftime(event.dtend, "%H:%M")
+
+            "#{event.summary}: from #{start_time} to #{end_time}"
+          end)
+    end
   end
 end
