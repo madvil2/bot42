fefb5c73d2987d0804e975b50b60e8da72c50c7c
KONSTANTIN KAIMOV <ikaimov98@gmail.com>
2023-12-11 00:43:28 +0100

    fix

--- lib/bot42/telegram.ex
+++ lib/bot42/telegram.ex
@@ -1,10 +1,8 @@
 defmodule Bot42.Telegram do
-  @spec send_message(integer(), String.t()) :: :ok
-  def send_message(chat_id, message) do
-    options = [parse_mode: "MarkdownV2"]
+    @spec send_message(integer(), String.t(), String.t()) :: :ok
+    def send_message(chat_id, message, opts \\ []) do
+      {:ok, _message} = Telegex.send_message(chat_id, message, opts)
 
-    {:ok, _message} = Telegex.send_message(chat_id, message, options)
-
-    :ok
-  end
+      :ok
+    end
 end
