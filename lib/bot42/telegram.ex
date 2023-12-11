c7610a9201c574719535c8f3ae16d7ea81a2c58a
KONSTANTIN KAIMOV <ikaimov98@gmail.com>
2023-12-11 01:12:32 +0100

    fix

--- lib/bot42/telegram.ex
+++ lib/bot42/telegram.ex
@@ -1,8 +1,9 @@
 defmodule Bot42.Telegram do
-    @spec send_message(integer(), String.t(), String.t()) :: :ok
-    def send_message(chat_id, message, opts \\ []) do
-      {:ok, _message} = Telegex.send_message(chat_id, message, opts)
+  @spec send_message(integer(), String.t(), keyword() | nil) :: :ok
+  def send_message(chat_id, message, opts \\ []) do
+    safe_message = Phoenix.HTML.Safe.to_iodata(message)
+    {:ok, _message} = Telegex.send_message(chat_id, safe_message, opts)
 
-      :ok
-    end
+    :ok
+  end
 end
