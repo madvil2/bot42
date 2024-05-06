24404602e466fbd9dd805fc0c4934d7a6e2ebf97
KONSTANTIN KAIMOV <ikaimov98@gmail.com>
2024-05-06 18:06:12 +0200

    fix

--- lib/bot42/telegram.ex
+++ lib/bot42/telegram.ex
@@ -3,13 +3,27 @@ defmodule Bot42.Telegram do
 
   @spec send_message(integer(), String.t(), keyword() | nil) :: :ok | {:error, term()}
   def send_message(chat_id, message, opts \\ []) do
-    case Telegex.send_message(chat_id, escape_telegram(message), opts) do
-      {:ok, _} ->
-        :ok
+    # Telegram's character limit for regular messages
+    max_length = 4096
 
-      {:error, reason} ->
-        log_and_notify_error(reason, %{chat_id: chat_id, message: message, opts: opts})
-        {:error, reason}
+    if String.length(message) > max_length do
+      # Split the message into chunks
+      chunks = Enum.chunk_every(message, max_length)
+
+      # Send each chunk
+      Enum.each(chunks, fn chunk ->
+        Telegex.send_message(chat_id, escape_telegram(chunk), opts)
+      end)
+    else
+      # Send the message as usual
+      case Telegex.send_message(chat_id, escape_telegram(message), opts) do
+        {:ok, _} ->
+          :ok
+
+        {:error, reason} ->
+          log_and_notify_error(reason, %{chat_id: chat_id, message: message, opts: opts})
+          {:error, reason}
+      end
     end
   end
 
